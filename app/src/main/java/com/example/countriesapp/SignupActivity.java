d8f5699ecaa1751db2cb76017ca2cf045a28b53b
Hafiz Furqan Irfan <43038504+leofurqan@users.noreply.github.com>
2023-12-12 22:00:07 +0500

    signup with backend

---
+++ app/src/main/java/com/example/countriesapp/SignupActivity.java
@@ -0,0 +1,95 @@
+package com.example.countriesapp;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.countriesapp.databinding.ActivitySignupBinding;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SignupActivity extends AppCompatActivity {
+
+    String url = "http://192.168.151.234/app/register.php";
+    ActivitySignupBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_signup);
+
+        binding.btnRegister.setOnClickListener(v -> {
+            if(validation()) {
+                saveUserData();
+            }
+        });
+    }
+
+    private void saveUserData() {
+        StringRequest request = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.d("r_user", response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("e_user", error.toString());
+            }
+        }) {
+            @Nullable
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                HashMap<String, String> params = new HashMap<>();
+                params.put("username", binding.etUsername.getText().toString());
+                params.put("email", binding.etEmail.getText().toString());
+                params.put("phone", binding.etPhone.getText().toString());
+                params.put("password", binding.etPassword.getText().toString());
+
+                return params;
+            }
+        };
+
+        Volley.newRequestQueue(this).add(request);
+    }
+
+    private boolean validation() {
+        if(TextUtils.isEmpty(binding.etUsername.getText().toString())) {
+            Toast.makeText(this, "Username is required", Toast.LENGTH_SHORT).show();
+            return false;
+        } else if(TextUtils.isEmpty(binding.etEmail.getText().toString())) {
+            Toast.makeText(this, "Email is required", Toast.LENGTH_SHORT).show();
+            return false;
+        } else if(TextUtils.isEmpty(binding.etPhone.getText().toString())) {
+            Toast.makeText(this, "Phone is required", Toast.LENGTH_SHORT).show();
+            return false;
+        } else if(TextUtils.isEmpty(binding.etPassword.getText().toString())) {
+            Toast.makeText(this, "Password is required", Toast.LENGTH_SHORT).show();
+            return false;
+        } else if(TextUtils.isEmpty(binding.etCPassword.getText().toString())) {
+            Toast.makeText(this, "Confirm Password is required", Toast.LENGTH_SHORT).show();
+            return false;
+        } else if(!binding.etPassword.getText().toString().equals(binding.etCPassword.getText().toString())) {
+            Toast.makeText(this, "Password doesn't match", Toast.LENGTH_SHORT).show();
+            return false;
+        } else if(binding.etPassword.getText().length() < 6) {
+            Toast.makeText(this, "Password too short", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        return true;
+    }
+}
